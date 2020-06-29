b06047388bef410d4315d68ea7a377f0c44357be
Tianle Chen <tianlechen@gmail.com>
2020-06-29 14:22:37 -0400

    size argument

--- webapitools.py
+++ webapitools.py
@@ -1,7 +1,8 @@
 import requests
 
-def get_entity_mentions(entity_name):
-    r = requests.get('https://api.pushshift.io/reddit/search/comment/?q={qterm}'.format(
-        qterm=entity_name
-    ))
-    return r.json()['data']
+def get_entity_mentions(entity_name, size=None):
+	q = 'https://api.pushshift.io/reddit/search/comment/?q={qterm}&size={size}'.format(
+		qterm=entity_name,
+		size=size if size is not None else 25
+	)
+    return requests.get(q).json()['data']
