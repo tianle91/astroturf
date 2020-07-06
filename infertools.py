cd60098f11bc306385d473face43adca4a8e7c12
Tianle Chen <tianlechen@gmail.com>
2020-07-05 22:50:38 -0400

    debug reddit context, num_return_sequences

--- infertools.py
+++ infertools.py
@@ -7,7 +7,7 @@ from transformers import GPT2Tokenizer, pipeline
 from data_user_comments import get_context, format_comment_as_json, format_submission_as_json
 from finetune import get_qa_string
 
-def make_package_comment(comment):
+def make_package_comment(comment, reddit):
     _, submission = get_context(comment, reddit)
     return {
         'comment': None,
@@ -31,7 +31,7 @@ def make_package_url(url, reddit):
     if isinstance(refobj, Submission):
         return make_package_submission(refobj)
     elif isinstance(refobj, Comment):
-        return make_package_comment(refobj)
+        return make_package_comment(refobj, reddit)
         
 def get_text_generation_pipeline(modeloutputpath):
     tokenizer = GPT2Tokenizer.from_pretrained('gpt2')
@@ -54,12 +54,12 @@ if __name__ == '__main__':
     # https://huggingface.co/transformers/_modules/transformers/pipelines.html#TextGenerationPipeline
     # https://huggingface.co/transformers/_modules/transformers/modeling_utils.html#PreTrainedModel.generate
     txtgen = get_text_generation_pipeline('finetune/{}/model/'.format(args.user))
-    responses = txtgen(prompt, max_length=len(prompt.split(' '))+128)
+    responses = txtgen(prompt, max_length=len(prompt.split(' '))+128, num_return_sequences=10)
 
     i = 0
     for response in responses:
         print ('response #[{}/{}]:\n\n{}'.format(
             i, len(responses), 
-            responses[0]['generated_text'].replace(prompt, '').strip().split('\n')[0]
+            response['generated_text'].replace(prompt, '').strip().split('\n')[0]
         ))
         i += 1
