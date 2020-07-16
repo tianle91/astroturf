f0966dbc89c64009ea5c488556f5ca755420b5eb
tianle <tianle@localhost.localdomain>
2020-07-16 17:12:59 -0400

    allow for multiple users as argument

--- data_user_comments.py
+++ data_user_comments.py
@@ -43,20 +43,12 @@ def format_submission_as_json(submission):
         
     }
 
-if __name__ == '__main__':
-
-    import argparse
-    parser = argparse.ArgumentParser(description='search comments by new for user.')
-    parser.add_argument('--user', type=str, default='suncoasthost')
-    parser.add_argument('--limit', type=int, default=1000)
-    args = parser.parse_args()
-
-    user_name = args.user
-    limit = args.limit
-
-    reddit = praw.Reddit()
+def dump_user_comments(user_name, reddit, limit=1000):
+    '''
+    dump user comments to data/user/{user_name}/*.json
+    and update data/user/{user_name}/manifest.csv with status
+    '''
     user = reddit.redditor(user_name)
-
     outpath = 'data/user/{}'.format(user_name)
     os.makedirs(outpath, exist_ok=True)
 
@@ -91,3 +83,18 @@ if __name__ == '__main__':
                 f.write('{}, {}\n'.format(comment.id, datetime.utcnow().isoformat()))
             with open(os.path.join(outpath, '{}.json'.format(comment.id)), 'w+') as f:
                 json.dump(package, f, indent=4)
+    return True
+
+
+if __name__ == '__main__':
+
+    import argparse
+    parser = argparse.ArgumentParser(description='search comments by new for user.')
+    parser.add_argument('--users', type=str, nargs='*')
+    parser.add_argument('--limit', type=int, default=1000)
+    args = parser.parse_args()
+
+    limit = args.limit
+    reddit = praw.Reddit()
+    for user_name in args.users:
+        print ('\n\nuser_name: {} done?: {}\n\n'.format(user_name, dump_user_comments(user_name, reddit, limit=1000)))
