a42aa7282fd25944245ac1b9495bfa4f2658da2e
Tianle Chen <tianlechen@gmail.com>
2020-08-02 13:51:34 -0400

    deprecate search function

--- astroturf/webapitools.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-def get_entity_mentions(entity_name, size=None):
-	q = 'https://api.pushshift.io/reddit/search/comment/?q={qterm}&size={size}'.format(
-		qterm=entity_name,
-		size=size if size is not None else 25
-	)
-	return requests.get(q).json()['data']
