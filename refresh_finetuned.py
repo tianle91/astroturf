b3465868c80d6e220af2bb5616f9e27104428efd
Tianle Chen <tianlechen@gmail.com>
2020-10-25 13:20:14 -0400

    big overhaul to use gcp_utils and path_config

--- refresh_finetuned.py
+++ refresh_finetuned.py
@@ -1,58 +1,63 @@
+import json
 import os
-from glob import glob
 
 from google.cloud import storage
 
-from astroturf.finetune import dump_finetuned
+from astroturf.finetune import dump_finetuned, model_output_fnames
+from gcp_utils import (download_all_cloud_files_with_prefix,
+                       upload_all_local_files_with_prefix)
 
+client = storage.Client()
+config_bucket = client.bucket('astroturf-dev-configs')
+path_config = json.loads(config_bucket.blob('pathConfig.json').download_as_string())
+data_bucket = path_config['data_bucket']
+cloud_data_path = path_config['data_path']
+local_data_path = 'data'
+model_bucket = path_config['model_bucket']
+cloud_model_path = path_config['model_path']
+local_model_path = 'models'
 
 def refresh_finetuned(
     user_name,
     blocksize=16,
     maxsteps=10,
-    force_update=False,
-    data_bucket='astroturf-dev-data',
-    data_prefix='prawtools/make_package_training/user/',
-    model_bucket='astroturf-dev-models',
-    model_prefix='finetune/dump_finetuned/user/',
+    force_update=False
 ):
-    client = storage.Client()
-    data_prefix_user = os.path.join(data_prefix, user_name)
-    os.makedirs(data_prefix_user, exist_ok=True)
+    cloud_model_path_user = os.path.join(cloud_model_path, user_name)
+    local_model_path_user = os.path.join(local_model_path, user_name)
+    os.makedirs(local_model_path_user, exist_ok=True)
+    cloud_data_path_user = os.path.join(cloud_data_path, user_name)
+    local_data_path_user = os.path.join(local_data_path, user_name)
+    os.makedirs(local_data_path_user, exist_ok=True)
 
-    model_prefix_user = os.path.join(model_prefix, user_name)
     bucket = client.bucket(model_bucket)
-    output_flags = [os.path.join(model_prefix_user, 'model/{}'.format(s)) for s in [
-        'pytorch_model.bin', 'config.json', 'training_args.bin'
-    ]]
+    output_flags = [os.path.join(cloud_model_path_user, s) for s in model_output_fnames]
 
     # skip if not force_update and model exists
     if not force_update and all(bucket.blob(s).exists() for s in output_flags):
         print ('Skip refresh_finetuned due to existing output_flags:\n{}'.format('\n'.join(output_flags)))
-        return False
-
+        return cloud_model_path_user
     # download files for training
-    has_training_data = False
-    for blob in client.list_blobs(data_bucket, prefix=data_prefix_user):
-        has_training_data = True
-        if not os.path.isfile(blob.name):
-            print ('Downloading: {}'.format(blob.name))
-            blob.download_to_filename(blob.name)
-    if not has_training_data:
-        print ('has_training_data: {}'.format(has_training_data))
-        return False
-
+    downloaded_data_fnames = download_all_cloud_files_with_prefix(
+        local_data_path_user,
+        data_bucket, cloud_data_path_user,
+        client, refresh_local=False
+    )
+    if not len(downloaded_data_fnames) > 0:
+        raise ValueError('no data for user_name: {}'.format(user_name))
+    # run the finetuning
+    # this guy dumps model files in os.path.join(local_model_path_user, 'model')
     dump_finetuned(
-        data_prefix_user, model_prefix_user,
+        local_data_path_user, local_model_path_user,
         blocksize=blocksize, max_steps=maxsteps
     )
-
     # upload results
-    for fname in glob(os.path.join(model_prefix_user, 'model/*')):
-        print ('Uploading: {}'.format(fname))
-        blob = bucket.blob(fname)
-        blob.upload_from_filename(fname)
-    return True
+    _ = upload_all_local_files_with_prefix(
+        os.path.join(local_model_path_user, 'model'),
+        model_bucket, os.path.join(cloud_model_path_user, 'model'),
+        client
+    )
+    return cloud_model_path_user
 
 if __name__ == '__main__':
 
