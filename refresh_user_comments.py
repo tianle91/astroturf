d2fda0ce549bf77744e0e5d0c5d4b15e823aa7b3
Tianle Chen <tianlechen@gmail.com>
2020-10-25 13:36:26 -0400

    refactor to use path_config

--- refresh_user_comments.py
+++ refresh_user_comments.py
@@ -6,26 +6,27 @@ from google.cloud import storage
 
 from astroturf.prawtools import make_package_training
 
+client = storage.Client()
+config_bucket = client.bucket('astroturf-dev-configs')
+path_config = json.loads(config_bucket.blob('pathConfig.json').download_as_string())
+data_bucket = path_config['data_bucket']
+data_path = path_config['data_path']
 
-def refresh_user_comments(
-    user_name, reddit, limit=1000,
-    prefix='prawtools/make_package_training/user/',
-    bucket='astroturf-dev-data',
-):
+def refresh_user_comments(user_name: str, reddit: praw.Reddit, limit: int = 1000):
     '''dump user comments to {gcp_bucket}/{prefix}/{user_name}/{comment_id}.json'''
-    client = storage.Client()
-    bucket = client.bucket(bucket)
-    prefix_user = os.path.join(prefix, user_name)
-    exist_blob_names = [blob.name for blob in client.list_blobs(bucket, prefix=prefix_user)]
+
+    bucket = client.bucket(data_bucket)
+    cloud_data_path_user = os.path.join(data_path, user_name)
+    exist_blob_paths = [blob.name for blob in client.list_blobs(bucket, prefix=cloud_data_path_user)]
     i = 0
     for comment in reddit.redditor(user_name).comments.new(limit=limit):
-        commentoutpath = os.path.join(prefix_user, '{}.json'.format(comment.id))
-        if not commentoutpath in exist_blob_names:
+        blob_path = os.path.join(cloud_data_path_user, '{}.json'.format(comment.id))
+        if not blob_path in exist_blob_paths:
             print('[{i}/{limit}] id: {id}, body: {body}'.format(
                 i=i, limit=limit, id=comment.id, body=comment.body.replace('\n', ' ').replace('\t', ' ')[:50]
             ))
             package = make_package_training(comment, reddit)
-            blob = bucket.blob(commentoutpath)
+            blob = bucket.blob(blob_path)
             blob.upload_from_string(json.dumps(package, indent=4))
         i += 1
     return True
