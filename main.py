23a38c1db3b423a3c1356e86b4db1d5cc4d3c2ae
Tianle Chen <tianlechen@gmail.com>
2020-10-25 13:36:37 -0400

    refactor to use praw_utils

--- main.py
+++ main.py
@@ -1,15 +1,14 @@
-import configparser
 import json
 import os
 
 import flask
-import praw
 from google.cloud import storage
 
 from astroturf.finetune import model_output_fnames
 from astroturf.infer import (get_qa_string, get_text_generation_pipeline,
                              make_package_infer_url)
 from gcp_utils import download_all_cloud_files_with_prefix
+from praw_utils import get_reddit
 
 client = storage.Client()
 config_bucket = client.bucket('astroturf-dev-configs')
@@ -34,12 +33,6 @@ def refresh_local_models(user_name, force_update=False):
         raise ValueError('{} has no model files.'.format(user_name))
     return local_model_path_user
 
-def get_reddit():
-    praw_blob = config_bucket.blob('praw.ini')
-    config = configparser.ConfigParser()
-    config.read_string(praw_blob.download_as_text())
-    return praw.Reddit(**config['DEFAULT'])
-
 def simulate_redditor_reponse(request: flask.Request):
     """HTTP Cloud Function. Returns response text or any valid input to
     https://flask.palletsprojects.com/en/1.1.x/api/#flask.make_response
@@ -50,7 +43,7 @@ def simulate_redditor_reponse(request: flask.Request):
         local_model_path_user = refresh_local_models(user_name)
     except Exception as e:
         return str(e)
-    reddit = get_reddit()
+    reddit = get_reddit(client, path_config['config_bucket'])
     txtgen = get_text_generation_pipeline(local_model_path_user)
     package = make_package_infer_url(request_json['url'], reddit)
     prompt = get_qa_string(package)
