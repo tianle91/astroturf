1fe78eb93e50f7038be7c29ca17d3a95ac22b905
Tianle Chen <tianlechen@gmail.com>
2020-10-29 22:49:44 -0400

    get user list from available models in cloud

--- ui.py
+++ ui.py
@@ -1,5 +1,7 @@
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 
+import json
+
 from flask import Flask, render_template, request
 from google.cloud import storage
 
@@ -9,14 +11,14 @@ from praw_utils import get_reddit
 app = Flask(__name__)
 client = storage.Client()
 reddit = get_reddit(client, 'astroturf-dev-configs')
+client = storage.Client()
+config_bucket = client.bucket('astroturf-dev-configs')
+path_config = json.loads(config_bucket.blob('pathConfig.json').download_as_string())
+model_bucket = path_config['model_bucket']
+cloud_model_path = path_config['model_path']
 
-with open('users.txt') as f:
-    users = f.read().split()
-
-local_model_path_user_d = {
-    user_name: refresh_local_models(user_name)
-    for user_name in users
-}
+users = [blob.name.replace(cloud_model_path, '') for blob in client.list_blobs(model_bucket, prefix=cloud_model_path)]
+local_model_path_user_d = {user_name: refresh_local_models(user_name) for user_name in users}
 
 defaulturl = 'https://www.reddit.com/r/toronto/comments/hkjyjn/city_issues_trespassing_orders_to_demonstrators/fwt4ifw'
 
