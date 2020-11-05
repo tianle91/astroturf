dae9ad840ba056798899b209d65b86dad839904a
Tianle Chen <tianlechen@gmail.com>
2020-11-04 22:46:25 -0500

    refactor common functionality to main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import json
 import os
+from typing import Optional
 
 import flask
 from google.cloud import storage
+from praw import Reddit
+from prawcore.exceptions import NotFound
 
 from astroturf.finetune import model_output_fnames, get_qa_string
 from astroturf.infer import get_text_generation_pipeline, make_package_infer_url
@@ -18,10 +21,36 @@ local_model_path = '/tmp/models/'
 reddit = get_reddit(client, 'astroturf-dev-configs')
 
 
-def refresh_local_models(user_name, force_update=False):
+def is_trained(username: str) -> bool:
+    users = sorted(list({
+        blob.name.replace(cloud_model_path, '').split('/')[0]
+        for blob in client.list_blobs(model_bucket, prefix=cloud_model_path)
+    }))
+    return username in users
+
+
+def is_invalid(username: str, r: Reddit) -> bool:
+    if len(username) == 0:
+        # not catched by NotFound (as of 2020-11-04)
+        return True
+    try:
+        r.redditor(name=username).id
+    except NotFound:
+        return True
+    return False
+
+
+def model_last_updated(username: str) -> Optional[str]:
+    if not is_trained(username):
+        return None
+    for blob in client.list_blobs(model_bucket, prefix=os.path.join(cloud_model_path, username)):
+        return blob.updated.strftime('%c')
+
+
+def refresh_local_models(username, force_update=False):
     """return local path for model files for user==user_name"""
-    cloud_model_path_user = os.path.join(cloud_model_path, user_name, 'model')
-    local_model_path_user = os.path.join(local_model_path, user_name, 'model')
+    cloud_model_path_user = os.path.join(cloud_model_path, username, 'model')
+    local_model_path_user = os.path.join(local_model_path, username, 'model')
     # skip refresh
     targets_exist = [os.path.isfile(os.path.join(local_model_path_user, fname)) for fname in model_output_fnames]
     if not force_update and all(targets_exist):
@@ -31,13 +60,13 @@ def refresh_local_models(user_name, force_update=False):
         local_model_path_user, model_bucket, cloud_model_path_user, client
     )
     if not len(downloaded_fnames) > 0:
-        raise ValueError('{} has no model files.'.format(user_name))
+        raise ValueError('{} has no model files.'.format(username))
     return local_model_path_user
 
 
-def simulate_redditor_response(user, url):
+def simulate_redditor_response(username, url):
     try:
-        local_model_path_user = refresh_local_models(user)
+        local_model_path_user = refresh_local_models(username)
     except Exception as e:
         return str(e)
     txtgen = get_text_generation_pipeline(local_model_path_user)
--- ui.py
+++ ui.py
@@ -1,16 +1,12 @@
 # https://www.digitalocean.com/community/tutorials/how-to-make-a-web-application-using-flask-in-python-3
 
 import json
-import os
-from typing import Optional
 
 from flask import Flask, render_template, request, flash, redirect, url_for
 from google.cloud import pubsub_v1
 from google.cloud import storage
-from praw import Reddit
-from prawcore.exceptions import NotFound
 
-from main import refresh_local_models, simulate_redditor_response
+from main import refresh_local_models, simulate_redditor_response, is_invalid, is_trained, model_last_updated
 from praw_utils import get_reddit
 
 app = Flask(__name__)
@@ -25,41 +21,13 @@ app.secret_key = config_bucket.blob('app_secret_key').download_as_string()
 path_config = json.loads(config_bucket.blob('pathConfig.json').download_as_string())
 model_bucket = path_config['model_bucket']
 cloud_model_path = path_config['model_path']
-data_bucket = path_config['data_bucket']
-cloud_data_path = path_config['data_path']
 project_id = path_config['project_id']
 
 # publishing refresh requests
 publisher = pubsub_v1.PublisherClient()
 topic_path = publisher.topic_path(project_id, 'model_refresh_requests')
 
-
-def is_trained(username: str) -> bool:
-    users = sorted(list({
-        blob.name.replace(cloud_model_path, '').split('/')[0]
-        for blob in client.list_blobs(model_bucket, prefix=cloud_model_path)
-    }))
-    return username in users
-
-
 defaulturl = 'https://www.reddit.com/r/toronto/comments/hkjyjn/city_issues_trespassing_orders_to_demonstrators/fwt4ifw'
-
-
-def model_last_updated(username: str) -> Optional[str]:
-    for blob in client.list_blobs(model_bucket, prefix=os.path.join(cloud_model_path, username)):
-        return blob.updated.strftime('%c')
-    return None
-
-
-def is_invalid(username: str, r: Reddit) -> bool:
-    if len(username) == 0:
-        # not catched by NotFound (as of 2020-11-04
-        return True
-    try:
-        r.redditor(name=username).id
-    except NotFound:
-        return True
-    return False
 
 
 @app.route('/', methods=('GET', 'POST'))
