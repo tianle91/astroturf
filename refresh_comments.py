9be28442514b7d94682ba771c51751e04c14e6d6
Tianle Chen <tianlechen@gmail.com>
2020-11-11 22:05:47 -0500

    use status flags

--- refresh_comments.py
+++ refresh_comments.py
@@ -5,6 +5,7 @@ import praw
 from google.cloud import storage
 
 from astroturf.prawtools import make_package_training
+from statusflags import StatusFlags
 
 client = storage.Client()
 config_bucket = client.bucket('astroturf-dev-configs')
@@ -16,7 +17,7 @@ status_bucket = client.bucket(path_config['status_bucket'])
 def refresh_user_comments(user_name: str, reddit: praw.Reddit, limit: int = 1000):
     '''dump user comments to {gcp_bucket}/{prefix}/{user_name}/{comment_id}.json'''
     cloud_data_path_user = user_name
-    status_progress = status_bucket.blob(os.path.join(user_name, '_UPDATE_REFRESH_PROGRESS'))
+    status_progress = status_bucket.blob(os.path.join(user_name, StatusFlags.data_refresh_progress))
     status_progress.upload_from_string('starting')
     exist_blob_paths = [blob.name for blob in client.list_blobs(data_bucket, prefix=cloud_data_path_user)]
     i = 0
@@ -32,7 +33,7 @@ def refresh_user_comments(user_name: str, reddit: praw.Reddit, limit: int = 1000
             blob.upload_from_string(json.dumps(package, indent=4))
         i += 1
     status_progress.delete()
-    status_success = status_bucket.blob(os.path.join(user_name, '_UPDATE_REFRESH_SUCCESS'))
+    status_success = status_bucket.blob(os.path.join(user_name, StatusFlags.data_refresh_success))
     status_success.upload_from_string('done')
     return True
 
