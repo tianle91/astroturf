9a651ba17e990f668f5f9997aef86918e2028871
Tianle Chen <tianlechen@gmail.com>
2020-11-28 21:36:17 -0500

    do some simple caching

--- infer.py
+++ infer.py
@@ -6,7 +6,8 @@ from fastapi import FastAPI
 from flask import Flask, flash, redirect, render_template, request, url_for
 from google.cloud import pubsub_v1, storage
 
-from main import simulate_redditor_response
+from main import (get_local_models, get_text_generation_pipeline,
+                  simulate_pipeline_response)
 from status import last_success
 
 app = FastAPI()
@@ -18,12 +19,29 @@ path_config = json.loads(config_bucket.blob(
     'pathConfig.json').download_as_string())
 defaulturl = path_config['defaulturl']
 
-cached_text_generation_pipelines = {}
+cached_txtgen = {}
+cached_max_count = 10
 
 
 @app.get("/{username}")
 def infer(username: str, url: str = defaulturl, refresh: bool = False):
-    return simulate_redditor_response(username, url, force_update=refresh)
+    if refresh or username not in cached_txtgen:
+        local_model_path_user = get_local_models(username, force_update=True)
+        txtgen = get_text_generation_pipeline(local_model_path_user)
+        cached_txtgen[username] = (txtgen, 1)
+        if len(cached_txtgen.keys()) > cached_max_count:
+            # pop the least used key
+            min_n = min(v[1] for k, v in cached_txtgen.items())
+            for k, n in cached_txtgen:
+                if n == min_n:
+                    cached_txtgen.pop(k)
+    else:
+        txtgen, n = cached_txtgen[username]
+        cached_txtgen[username] = (txtgen, n+1)
+    response = simulate_pipeline_response(txtgen, url)
+    print (cached_txtgen)
+    return response
+
 
 # holy shit this is indeed fast
 # uvicorn infer:app --reload
