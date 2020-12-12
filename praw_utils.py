65d840e5191416235e0cc5b12f09debe957551e2
Tianle Chen <tianlechen@gmail.com>
2020-12-12 18:53:42 -0500

    set valid default

--- praw_utils.py
+++ praw_utils.py
@@ -4,7 +4,7 @@ import praw
 from google.cloud import storage
 
 
-def get_reddit(client: storage.Client, config_bucket: str, site: str = 'DEFAULT') -> praw.Reddit:
+def get_reddit(client: storage.Client, config_bucket: str, site: str = 'chennngiskhan') -> praw.Reddit:
     bucket = client.bucket(config_bucket)
     praw_blob = bucket.blob('praw.ini')
     config = configparser.ConfigParser()
