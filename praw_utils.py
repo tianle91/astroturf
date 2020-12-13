0bb29265f647417dc188a6a98eb2823081017533
Tianle Chen <tianlechen@gmail.com>
2020-12-13 18:09:43 -0500

    astroturf-praw.ini

--- praw_utils.py
+++ praw_utils.py
@@ -4,9 +4,9 @@ import praw
 from google.cloud import storage
 
 
-def get_reddit(client: storage.Client, config_bucket: str, site: str = 'chennngiskhan') -> praw.Reddit:
+def get_reddit(client: storage.Client, config_bucket: str, site: str = 'tianle91astroturf') -> praw.Reddit:
     bucket = client.bucket(config_bucket)
-    praw_blob = bucket.blob('praw.ini')
+    praw_blob = bucket.blob('astroturf-praw.ini')
     config = configparser.ConfigParser()
     config.read_string(praw_blob.download_as_text())
     return praw.Reddit(**config[site])
