f6b5fa40fdb42675c2606a05a12bb64d4ff9895d
Tianle Chen <tianlechen@gmail.com>
2020-12-22 10:30:02 -0500

    add update status messages

--- update_worker.py
+++ update_worker.py
@@ -14,10 +14,15 @@ path_config = json.loads(config_bucket.blob(
 project_id = path_config['project_id']
 infer_endpoint = path_config['infer_endpoint']
 
-# subscribing to refresh requests
+# subscribing to sub_update_request
 subscriber = pubsub_v1.SubscriberClient()
 subscription_path = subscriber.subscription_path(
     project_id, path_config['sub_update_request'])
+
+# publishing to pub_update_status
+publisher = pubsub_v1.PublisherClient()
+topic_path = publisher.topic_path(
+    project_id, path_config['pub_update_status'])
 
 if __name__ == '__main__':
     import argparse
@@ -46,12 +51,26 @@ if __name__ == '__main__':
 
         ack_ids = [msg.ack_id for msg in response.received_messages]
         if len(ack_ids) > 0 and username is not None:
+            # publish to pub_update_status
+            future = publisher.publish(
+                topic_path,
+                data=str.encode(username),
+                status=str.encode('received_request')
+            )
+            message_id = future.result()
             # there's something to do! ack it to remove from queue.
             subscriber.acknowledge(
                 request={"subscription": subscription_path, "ack_ids": ack_ids})
             # update comments
             refreshed_comment_ids = refresh_user_comments_cloud(
                 username, reddit, limit=args.limit)
+            # publish to pub_update_status
+            future = publisher.publish(
+                topic_path,
+                data=str.encode(username),
+                status=str.encode('comments_refreshed')
+            )
+            message_id = future.result()
             if len(refreshed_comment_ids) > 0:
                 # update finetuned
                 username_finedtuned = refresh_finetuned_cloud(
@@ -61,7 +80,30 @@ if __name__ == '__main__':
                     force_update=args.forceupdate
                 )
                 # update model at infer endpoint
-                inferresponse = requests.get('{infer_endpoint}/{username}?refresh=True'.format(
+                inferresponse = requests.put('{infer_endpoint}/{username}'.format(
                     infer_endpoint=infer_endpoint,
                     username=username
                 )).json()
+                # publish to pub_update_status
+                future = publisher.publish(
+                    topic_path,
+                    data=str.encode(username),
+                    status=str.encode('finetuned')
+                )
+                message_id = future.result()
+            else:
+                # publish to pub_update_status
+                future = publisher.publish(
+                    topic_path,
+                    data=str.encode(username),
+                    status=str.encode('no_new_comments')
+                )
+                message_id = future.result()
+            # done!
+            # publish to pub_update_status
+            future = publisher.publish(
+                topic_path,
+                data=str.encode(username),
+                status=str.encode('success')
+            )
+            message_id = future.result()
