28bf47dc937f2b829c55fef3673e1c414e76b2b2
Tianle Chen <tianlechen@gmail.com>
2021-06-28 00:35:00 -0400

    better parser

--- astroturf/parser.py
+++ astroturf/parser.py
@@ -5,7 +5,9 @@ from typing import Optional, Tuple
 def parse_comment_body(s: str) -> Tuple[bool, Optional[str]]:
     """Return whether comment is relevent and mentioned username.
     """
-    if re.match('.*u/[a-zA-Z0-9]*\s(think|say|do)?', s, flags=re.IGNORECASE):
-        return True, re.search('u/[a-zA-Z0-9]*', s).group(0).split('u/')[1]
+    regex_str_username = r'(.|\s)*u/[a-zA-Z0-9_]*\s'
+    regex_str_trigger = regex_str_username + r'(think|say|do)?'
+    if re.match(regex_str_trigger, s, flags=re.IGNORECASE):
+        return True, re.search(regex_str_username, s).group(0).split('u/')[1].strip()
     else:
         return False, None
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,8 +1,31 @@
 from astroturf.parser import parse_comment_body
+import pytest
 
 
-def test_parse_comment_body():
-    assert parse_comment_body('u/username ?') == (True, 'username')
-    assert parse_comment_body(
-        'what would u/UserName say?') == (True, 'UserName')
-    assert not parse_comment_body('bullshit')[0]
+@pytest.mark.parametrize(
+    ('s', 'expected_username'),
+    [
+        pytest.param('u/username ?', 'username'),
+        pytest.param(' u/username ?', 'username'),
+        pytest.param('u/username say?', 'username'),
+        pytest.param('what would u/username say?', 'username'),
+        pytest.param('u/username1 ?', 'username1'),
+        pytest.param('u/user_name ?', 'user_name'),
+    ]
+)
+def test_parse_comment_body_relevant(s, expected_username):
+    res = parse_comment_body(s)
+    assert res[0]
+    actual_username = res[1]
+    assert expected_username == actual_username, actual_username
+
+
+@pytest.mark.parametrize(
+    ('s'),
+    [
+        pytest.param('bullshit'),
+        pytest.param('u/username?'),
+    ]
+)
+def test_parse_comment_body_not_relevant(s):
+    assert not parse_comment_body(s)[0]
